415d4a7325387dfffb28ef9af4a0e4f10a0c46d3
CarlosDevSys <cdealmeidavieira@gmail.com>
2024-06-11 00:09:40 -0300

    Finalizadas classes e main.

--- PooJava/src/Main.java
+++ PooJava/src/Main.java
@@ -1,5 +1,46 @@
+import br.com.carlosDevsys.desafio.dominio.BootCamp;
+import br.com.carlosDevsys.desafio.dominio.Curso;
+import br.com.carlosDevsys.desafio.dominio.Dev;
+import br.com.carlosDevsys.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso de Java");
+        curso1.setDescricao("Descrição do curso de java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso Python");
+        curso2.setDescricao("Curso avançado de python");
+        curso2.setCargaHoraria(10);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("descrição mentoria de Java");
+        mentoria.setData(LocalDate.now());
+
+        BootCamp bootcamp = new BootCamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição bootcamp java");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devJoao = new Dev();
+        devJoao.setNome("João");
+        devJoao.inscreverBootCamp(bootcamp);
+        System.out.println("Conteudos Inscritos " + devJoao.getNome() + ": " + devJoao.getConteudosInscritos());
+        devJoao.progredir();
+        devJoao.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos Inscritos " + devJoao.getNome() + ": " + devJoao.getConteudosInscritos());
+        System.out.println("Conteudos Concluidos " + devJoao.getNome() + ": " + devJoao.getConteudosConcluidos());
+        System.out.println("XP:" + devJoao.calcularTotalXp());
+
+
     }
 }
--- PooJava/src/br/com/carlosDevsys/desafio/dominio/Dev.java
+++ PooJava/src/br/com/carlosDevsys/desafio/dominio/Dev.java
@@ -1,8 +1,6 @@
 package br.com.carlosDevsys.desafio.dominio;
 
-import java.util.LinkedHashSet;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 
 public class Dev {
 
@@ -24,5 +22,51 @@ public class Dev {
         }else {
             System.err.println("Você não está matriculado em nenhum conteudo!");
         }
+    }
+    public double calcularTotalXp(){
+        Iterator<Conteudo> iterator = this.conteudosConcluidos.iterator();
+        double soma = 0;
+        while (iterator.hasNext()){
+            double next = iterator.next().calcularXp();
+            soma += next;
+        }
+        return soma;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Set<Conteudo> getConteudosInscritos() {
+        return conteudosInscritos;
+    }
+
+    public void setConteudosInscritos(Set<Conteudo> conteudosInscritos) {
+        this.conteudosInscritos = conteudosInscritos;
+    }
+
+    public Set<Conteudo> getConteudosConcluidos() {
+        return conteudosConcluidos;
+    }
+
+    public void setConteudosConcluidos(Set<Conteudo> conteudosConcluidos) {
+        this.conteudosConcluidos = conteudosConcluidos;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dev dev = (Dev) o;
+        return Objects.equals(nome, dev.nome) && Objects.equals(conteudosInscritos, dev.conteudosInscritos) && Objects.equals(conteudosConcluidos, dev.conteudosConcluidos);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, conteudosInscritos, conteudosConcluidos);
     }
 }
