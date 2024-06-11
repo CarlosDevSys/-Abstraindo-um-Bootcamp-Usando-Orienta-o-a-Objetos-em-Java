a1866f94d86223e5a06ce46b075a111325ff780f
CarlosDevSys <cdealmeidavieira@gmail.com>
2024-06-10 21:18:00 -0300

    Projeto e classes criadas.

---
+++ PooJava/src/br/com/carlosDevsys/desafio/dominio/Dev.java
@@ -0,0 +1,28 @@
+package br.com.carlosDevsys.desafio.dominio;
+
+import java.util.LinkedHashSet;
+import java.util.Optional;
+import java.util.Set;
+
+public class Dev {
+
+    private String nome;
+    private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
+    private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
+
+    public void inscreverBootCamp(BootCamp bootCamp){
+        this.conteudosInscritos.addAll(bootCamp.getConteudos());
+        bootCamp.getDevsInscritos().add(this);
+    }
+
+    public void progredir(){
+        Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+
+        if(conteudo.isPresent()){
+            this.conteudosConcluidos.add(conteudo.get());
+            this.conteudosInscritos.remove(conteudo.get());
+        }else {
+            System.err.println("Você não está matriculado em nenhum conteudo!");
+        }
+    }
+}
